0e8e707fe8c7e4a68085a25e53c05b235573a9cf
Komarova1997 <44423432+Komarova1997@users.noreply.github.com>
2019-04-27 10:54:18 +0700

    Update booking.py

--- booking.py
+++ booking.py
@@ -2,54 +2,69 @@ import random
 
 
 class Room:
-    '''Класс номеров
-    (places - количество мест в номере,
-    comfort - степень комфорта,
-    price - стоимость номера)'''
-    def __init__(self, places, comfort):
-        self._places = places
-        self._comfort = comfort
-        self._price = places * comfort
+    places = [2900, 2300, 3200, 4100]
+    comfort = [1.0, 1.2, 1.5]
 
-    def __str__(self):
-        return self._price
+    def __init__(self):
+        super(Room, self).__init__(places=Room.places,
+                                   comfort=Room.comfort)
 
-    def __repr__(self):
-        return self.__str__()
-
-    def get_price(self):
-        return self._price
-
-    @staticmethod
-    def random():
-        main_list = [1, 2, 3, 4]
-        answer = random.choice(main_list)
-        return answer
+    def booking_variants(self):
+        room_list = Room.reader('fund.txt')
+        list_room = []
+        for i in room_list:
+            i = i.split()
+            if 'одноместный' in i:
+                if 'стандарт' in i:
+                    i.append(self.places[0]*self.comfort[0])
+                if 'стандарт_улучшенный' in i:
+                    i.append(self.places[0]*self.comfort[1])
+                if 'апартамент' in i:
+                    i.append(self.places[0]*self.comfort[2])
+            if 'двухместный' in i:
+                if 'стандарт' in i:
+                    i.append(self.places[1]*self.comfort[0])
+                if 'стандарт_улучшенный' in i:
+                    i.append(self.places[1]*self.comfort[1])
+                if 'апартамент' in i:
+                    i.append(self.places[1]*self.comfort[2])
+            if 'полулюкс' in i:
+                if 'стандарт' in i:
+                    i.append(self.places[2] * self.comfort[0])
+                if 'стандарт_улучшенный' in i:
+                    i.append(self.places[2] * self.comfort[1])
+                if 'апартамент' in i:
+                    i.append(self.places[2] * self.comfort[2])
+            if 'люкс' in i:
+                if 'стандарт' in i:
+                    i.append(self.places[3] * self.comfort[0])
+                if 'стандарт_улучшенный' in i:
+                    i.append(self.places[3] * self.comfort[1])
+                if 'апартамент' in i:
+                    i.append(self.places[3] * self.comfort[2])
+            list_room += [i]
+        main_list = []
+        for i in list_room:
+            c = i.copy()
+            d = i.copy()
+            c[4] += 280.0
+            c.append('завтрак')
+            d[4] += 1000.0
+            d.append('полупансион')
+            i.append('без питания')
+            main_list += [[i, c, d, ['*']*31]]
+        return main_list
 
     @staticmethod
     def reader(file):
         main_list = []
         with open(file, 'r') as f:
             for i in f.readlines():
-                main_list += [i[:-1]]
+                main_list += [i]
         return main_list
 
     @staticmethod
-    def price_for_client(my_list, my_number):
-        return min(my_list, key=lambda x: abs(x - my_number))
-
-class Booking():
-    '''Класс размещения
-    (Информация о номере и о питании)'''
-    def __init__(self, _price, food):
-        self._food = food
-        self._main_price = _price + food
-
-    def __str__(self):
-        return str(self._main_price)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def get_main_price(self):
-        return self._main_price
+    def random():
+        main_list = [1, 2, 3, 4]
+        answer = random.choice(main_list)
+        return answer
