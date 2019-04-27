26edae1dece53d0016a867674835201c59af8208
Komarova1997 <44423432+Komarova1997@users.noreply.github.com>
2019-04-27 10:53:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,54 +2,27 @@ from booking import *
 
 
 def main():
-    random = Room.random()
+    fund_list = Room.booking_variants(Room)
     client_list = Room.reader('booking.txt')
-    room_list = Room.reader('fund.txt')
-    main_dict = {}
-    for i in room_list:
-        places = 0
-        comfort = 0
-        a, b, c, d = i.split()
-        if c == '1':
-            places = 2900
-        elif c == '2':
-            places = 2300
-        elif c == '3':
-            if b == 'полулюкс':
-                places = 3200
-            else:
-                places = 4100
-        if d == 'стандарт':
-            comfort = 1.0
-        elif d == 'стандарт_улучшенный':
-            comfort = 1.2
-        elif d == 'апартамент':
-            comfort = 1.5
-        e = Room.get_price(Room(places, comfort))
-        main_dict[a] = [b, c, d, e]
-    for i in main_dict.keys():
-        main_dict[i].append(Booking(main_dict[i][3], 280))
-        main_dict[i].append(Booking(main_dict[i][3], 1000))
-        main_dict[i].append('свободен')
-    print(main_dict)
-    print(client_list)
-    main_list = []
+    list_1 = []
     for i in client_list:
-        a,b,c,d,e,f,g,h = i.split()
-        main_list += [[a,b,c,d,e,f,g,h]]
+        client_list[client_list.index(i)] = i[:-1]
+        a, b, c, d, e, f, g, h = i.split()
+        list_1 += [[a, b, c, d, int(e), f, int(g), float(h)]]
+    print(fund_list)
+    variants = []
+    main_list = []
+    for client in list_1:
+        for room in fund_list:
+            if room[0][4] <= client[-1]:
+                variants += [room[0]]
+            if room[1][4] <= client[-1]:
+                variants += [room[1]]
+            if room[2][4] <= client[-1]:
+                variants += [room[2]]
+        main_list += [[client, variants]]
+        variants = []
     print(main_list)
-    list_room = []
-    for i in main_dict.values():
-        list_room += [i]
-        
-    '''list_room - список номеров с тремя значениями стоимости (в зависимости от питания)
-        main_list - список посетителей'''
-
-    # TODO: доработать
-    if random == 1:
-        print('Клиент отказался от варианта')
-    else:
-        print('Клиент согласен. Номер забронирован')
 
 
 if __name__ == '__main__':
